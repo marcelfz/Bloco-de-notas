d093fb3edc4600f5ccdd4cbaeb162d0a850f9bdc
marcelfz <marceloferraz60447@gmail.com>
2025-04-23 10:44:08 -0300

    008

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+// auth routes
+
+//sempre que eu entrar no endereço login, vou querer algo que exista na classe do authcontroler (um método e iremos passar o login que é o método)
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
