a98103b79936da93dde43ee4ac554e7cb7fdbca1
marcelfz <marceloferraz60447@gmail.com>
2025-04-23 13:24:35 -0300

    010

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 //sempre que eu entrar no endereço login, vou querer algo que exista na classe do authcontroler (um método e iremos passar o login que é o método)
 Route::get('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
