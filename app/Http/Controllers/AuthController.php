b8950536ec7f59df3018a333cb67172d1c41014b
marcelfz <marceloferraz60447@gmail.com>
2025-04-23 14:26:02 -0300

    014

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,10 +18,19 @@ class AuthController extends Controller
 
         // form validation
         $request->validate(
+            // rules
            [
-            'text_username' => 'required',
-            'text_password' => 'required',
-           ] 
+            'text_username' => 'required|email',
+            'text_password' => 'required|min:6|max:16',
+           ], 
+           // error messages
+           [
+            'text_username.required' => "O username é obrigatório",
+            'text_username.email' => "Username deve ser um email válido",
+            'text_password.required' => "O password é obrigatório",
+            'text_password.min' => "O password deve ter pelo menos :min  caracteres",
+            'text_password.max' => "O password deve ter no máximo :max  caracteres",
+           ]
         );
 
         // get user input
