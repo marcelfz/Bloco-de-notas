5d9f33011e11050ae6f2731c100645cea2d7146c
marcelfz <marceloferraz60447@gmail.com>
2025-04-23 10:02:50 -0300

    004

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,6 @@ Route::get('/about', function(){
     echo "About us";
 });
 
-Route::get('/main', [MainController::class, 'index']); // rota que aponta para um método [esse método agora está apontando para um view]
+Route::get('/main/{value}', [MainController::class, 'index']); // rota que aponta para um método [esse método agora está apontando para um view]
+
+// receber parâmetros nas Rotas/routes
