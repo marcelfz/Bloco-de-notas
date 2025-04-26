30e9d6c6019bae9560de066e6d3e1c6b59f1b219
marcelfz <marceloferraz60447@gmail.com>
2025-04-26 14:26:44 -0300

    021

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    
+    public function index(){
+        echo "I am inside the app";
+    }
+
+    public function newNote(){
+        echo "I am creating a new note";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged;
 use Illuminate\Support\Facades\Route;
 
 // auth routes
 
 //sempre que eu entrar no endereço login, vou querer algo que exista na classe do authcontroler (um método e iremos passar o login que é o método)
 Route::get('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout']);
 Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+
+Route::middleware([CheckIsLogged::class])->group(function(){
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/newNote', [MainController::class, 'newNote']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
