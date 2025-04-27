c5f7983e6fba1590beeb2850025681384efbd462
marcelfz <marceloferraz60447@gmail.com>
2025-04-27 15:52:57 -0300

    026

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index(){
         // load users notes
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
+
+        echo '<pre>';
+        print_r($user);
+        print_r($notes);
+
+        die();
 
         // show home view
         return view('home');
